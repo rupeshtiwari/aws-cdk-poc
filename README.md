870289a8b143fb820fb3667a0bf75878c87c96be
Rupesh Tiwari <roopkt@gmail.com>
2022-04-13 16:55:20 -0400

    updates

--- README.md
+++ README.md
@@ -17,3 +17,10 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 * `cdk deploy`      deploy this stack to your default AWS account/region
 * `cdk diff`        compare deployed stack with current state
 * `cdk synth`       emits the synthesized CloudFormation template
+
+## References
+- `git pull && git add . && git commit -m 'updates' && git push`
+
+- https://medium.com/@kargawal.abhishek/aws-cdk-deploy-managed-etl-using-aws-glue-job-1925098ec40f 
+
+- https://datachef.co/blog/deploy-pythonshell-gluejob-using-cdk/
--- lib/cdk-poc-stack.ts
+++ lib/cdk-poc-stack.ts
@@ -1,5 +1,21 @@
-import { aws_iam as iam, aws_s3 as s3, Stack, StackProps } from 'aws-cdk-lib';
+import {
+  aws_iam as iam,
+  aws_s3 as s3,
+  Stack,
+  StackProps,
+  aws_glue as glue,
+  aws_s3_deployment as s3deploy,
+  RemovalPolicy,
+} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as path from 'path';
+
+const GLUE_VERSION = '3.0';
+
+//This value must be glueetl for Apache Spark
+const COMMAND_NAME = 'glueetl';
+const SPARKAVRO_PATH = 'jars/spark-avro_2.12-3.1.2.jar';
+const BUCKET_NAME = 'octank-poc-bucket';
 
 export class PocStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -9,7 +25,48 @@ export class PocStack extends Stack {
 
   createStack() {
     const role = this.createGlueRole();
-    this.assignPermisssion(role);
+    const bucket = this.createS3Bucket();
+    this.uploadFilesToS3(bucket);
+    this.assignPermission(bucket, role);
+    this.createGlueJob(bucket, role);
+  }
+
+  private createGlueJob(bucket: s3.Bucket, role: iam.Role) {
+    const bucketName = bucket.bucketName;
+    const streamKafkaEventJobName = 'stream-kafka-events';
+    const streamKafkaEventJob = new glue.CfnJob(this, streamKafkaEventJobName, {
+      name: streamKafkaEventJobName,
+      role: role.roleArn,
+      command: {
+        name: COMMAND_NAME,
+        scriptLocation: `s3://${bucketName}/spark/streaming.scala`,
+      },
+      glueVersion: GLUE_VERSION,
+      defaultArguments: {
+        '--extra-jars': `s3://${bucketName}/${SPARKAVRO_PATH}`,
+        '--executor-cores': 8,
+      },
+      numberOfWorkers: 30,
+    });
+  }
+
+  private createS3Bucket() {
+    const myBucket = new s3.Bucket(this, 'MyCdkGlueJobBucket', {
+      versioned: true,
+      bucketName: 'octank-sdp-job-bucket',
+      removalPolicy: RemovalPolicy.RETAIN,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    });
+
+    return myBucket;
+  }
+
+  private uploadFilesToS3(bucket: s3.Bucket) {
+    new s3deploy.BucketDeployment(this, 'DeployGlueJobFiles', {
+      sources: [s3deploy.Source.asset(path.join(__dirname, '../assets'))],
+      destinationBucket: bucket,
+      destinationKeyPrefix: 'assets',
+    });
   }
 
   private createGlueRole() {
@@ -27,16 +84,7 @@ export class PocStack extends Stack {
     return role;
   }
 
-  private assignPermisssion(role: iam.Role) {
-    const dataBucketName = 'octank-poc-bucket';
-
-    // Assign permission to data bucket
-    const dataS3Bucket = s3.Bucket.fromBucketName(
-      this,
-      'existingBucket',
-      dataBucketName
-    );
-
-    dataS3Bucket.grantReadWrite(role);
+  private assignPermission(bucket: s3.Bucket, role: iam.Role) {
+    bucket.grantReadWrite(role);
   }
 }
